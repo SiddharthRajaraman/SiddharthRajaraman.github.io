4802f39bfd10354db9564b7de82c092bbfb9a7c6
Siddharth Rajaraman <sidrajaraman@gmail.com>
2021-06-12 20:26:52 -0400

    commit (Jun. 12)

--- src/mainJS.js
+++ src/mainJS.js
@@ -1,5 +1,23 @@
-function smoothScroll() {
+function smoothScrollProjects() {
   var element = document.querySelector(".cardView");
+
+  // scroll to element
+  element.scrollIntoView();
+}
+function smoothScrollAboutMe() {
+  var element = document.querySelector(".aboutMe");
+
+  // scroll to element
+  element.scrollIntoView();
+}
+function smoothScrollGoUp() {
+  var element = document.querySelector(".goUp");
+
+  // scroll to element
+  element.scrollIntoView();
+}
+function smoothScrollContact() {
+  var element = document.querySelector(".contactMe");
 
   // scroll to element
   element.scrollIntoView();
